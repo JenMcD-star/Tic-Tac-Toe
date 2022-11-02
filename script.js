e04781aa09e0a346b9f5e201205dce5e6320a2cc
JenMcD-star <jenniferannvancura@gmail.com>
2022-11-02 15:18:52 -0500

    Working on only allowing one click per turn.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 /* 1. create board (grid) 
 2. make option to choose x or o
 3. make choice appear when grid square is selected
+    -only one turn at a time
+    -computer turn
 4. stop game when 3 of the same symbols are in a row(not sure on this part!)
 5. alert winner
 6. return to blank grid
@@ -10,7 +12,7 @@ function makeBoard(num) {
   for (i = 0; i < num; i++) {
     let grid = document.getElementById("grid");
     let div = document.createElement("div");
-    div.id = `grid ${i}`;
+    div.id = `item ${i}`;
     grid.appendChild(div);
   }
 }
@@ -24,24 +26,60 @@ roundPicker.oninput = () => {
 };
 
 let userChoice;
+let computerChoice;
 const xButton = document.getElementById("xButton");
 const oButton = document.getElementById("oButton");
+const divs = document.querySelectorAll("[id*=item");
 
 function disableButton(btn) {
   document.getElementById(btn.id).disabled = true;
 }
 oButton.addEventListener("click", (event) => {
-  let userChoice = "O";
-  console.log(userChoice);
+  userChoice = "O";
+  computerChoice = "X";
   disableButton(oButton);
   disableButton(xButton);
   document.getElementById("range").disabled = true;
+  playGame();
 });
 
 xButton.addEventListener("click", (event) => {
-  let userChoice = "X";
+  userChoice = "X";
+  computerChoice = "O";
   console.log(userChoice);
   disableButton(oButton);
   disableButton(xButton);
   document.getElementById("range").disabled = true;
+  playGame();
 });
+
+function playGame() {
+  let playerturns = 0;
+  let computerturns = 0;
+  if (playerturns <= computerturns) {
+    playerPlay();
+  } else {
+    computerPlay();
+  }
+}
+
+function fillDivs() {
+  for (let i = 0; i < divs.length; i++) {
+    return divs[i].addEventListener("click", clickHandler);
+  }
+}
+
+function clickHandler(e) {
+  e.currentTarget.innerHTML = userChoice;
+}
+
+const randomNum = Math.floor(Math.random() * 9);
+
+function computerPlay(randomNum) {
+  console.log("rest");
+  divs[randomNum].innerHTML = `${computerChoice}`;
+}
+
+function playerPlay() {
+  fillDivs(userChoice);
+}
